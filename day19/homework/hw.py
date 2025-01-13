3475628fee5dafdc2f4e806f7559f8a92191ea89
niako117o <niasimonia1@gmail.com>
2025-01-13 21:01:30 +0400

    19th commit

---
+++ day19/homework/hw.py
@@ -0,0 +1,3 @@
+#inline css - ელემენტის styleshi რომ ვიყენებთ css არის inline
+#extrenal - როცა ცალკე css ფაილს ვქმნით და ლინკით ვაკავშირებთ
+#internal  - სანამ head და body დაიწყება მანდ styles ჩაშენება
