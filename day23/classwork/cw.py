5fc33f8b0747fbc02f1fd8779763d6dd3bf9d6bf
niako117o <niasimonia1@gmail.com>
2025-03-23 22:23:40 +0400

    24th commit

---
+++ day23/classwork/cw.py
@@ -0,0 +1,29 @@
+#1)
+numbers = [10, 20, 30, 40, 50]
+
+first_element = numbers[0]
+last_element = numbers[-1]
+middle_element = numbers[len(numbers) // 2]
+
+print(first_element)
+print(last_element)
+print(middle_element)
+
+#2)
+num = [1, 3, 3, 4, 5, 6, 6, 8, 9, 12]
+num[2] = 2 
+num[6] = 7 
+print(num)
+
+#3)
+animals = ["cat", "dog", "elephant", "lion", "tiger"]
+dog_index = animals.index("dog")
+print(dog_index)
+
+#4)GOA IS THE BEST ACADEMY
+words = ["GOA", "MAGARIA", "NOVATORI", "IS", "THE", "VIRGIN", "CHAD", "BEST", "ACADEMY"]
+correct_order = ["GOA", "IS", "THE", "BEST", "ACADEMY"]
+result = " ".join(correct_order)  
+print(result)
+
+
